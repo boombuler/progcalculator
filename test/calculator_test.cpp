e86e11e3b303b2cf1e4dface46c9d4268fffcf9f
boombuler <boombuler@gmail.com>
2024-08-05 12:51:19 +0200

    Added test for initial state

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -18,13 +18,10 @@ string calc(string input, int base = 10) {
   return calc.display;
 }
 
-void test_addition(void) {
-  TEST_ASSERT_EQUAL_STRING("2", calc("1+1").c_str());
-  TEST_ASSERT_EQUAL_STRING("44", calc("12+32").c_str());
-}
-
-void test_order_of_operation(void) {
-  TEST_ASSERT_EQUAL_STRING("6", calc("1+2*4-3").c_str());
+void test_initial_state(void) {
+  Calculator c;
+  TEST_ASSERT_EQUAL(Calculator::Mode::Dec, c.mode());
+  TEST_ASSERT_EQUAL_STRING("0", c.display.c_str());
 }
 
 void test_binary_output(void){
@@ -34,8 +31,18 @@ void test_binary_output(void){
   TEST_ASSERT_EQUAL_STRING("1100", calc("12", 2).c_str());
 }
 
+void test_addition(void) {
+  TEST_ASSERT_EQUAL_STRING("2", calc("1+1").c_str());
+  TEST_ASSERT_EQUAL_STRING("44", calc("12+32").c_str());
+}
+
+void test_order_of_operation(void) {
+  TEST_ASSERT_EQUAL_STRING("6", calc("1+2*4-3").c_str());
+}
+
 int runUnityTests(void) {
   UNITY_BEGIN();
+  RUN_TEST(test_initial_state);
   RUN_TEST(test_binary_output);
   RUN_TEST(test_addition);
   RUN_TEST(test_order_of_operation);
